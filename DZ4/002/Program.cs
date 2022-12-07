754e2845aa7142df7af6336ab6ddee5e858fa5e1
Maksim Molyanov <fioklit@gmail.com>
2022-12-07 20:34:38 +0400

    DZ2 task 2

---
+++ DZ4/002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num = int.Parse(Console.ReadLine());
+
+int sum = SumOfDigits(num);
+Console.WriteLine(sum);
+
+
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    while(num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
