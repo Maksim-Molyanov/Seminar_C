9bab3c32f5103de94e1177b04b0cf7674d3e9f18
Maksim Molyanov <fioklit@gmail.com>
2022-12-25 19:56:31 +0400

    DZ9 Recursion

---
+++ DZ9/001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+printNum(n);
+
+void printNum(int i)
+{
+    if(i < 1)
+    {
+        return;
+    }
+    Console.WriteLine(i);
+    printNum(i -1);
+}
